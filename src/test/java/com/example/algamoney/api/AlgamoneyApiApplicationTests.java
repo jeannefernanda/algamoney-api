1522348afb79f83c72adb4d846b7c3db42bd79cb
Jeanne <jeannefernanda.m@gmail.com>
2023-07-22 15:15:27 -0400

    Projeto criado

---
+++ src/test/java/com/example/algamoney/api/AlgamoneyApiApplicationTests.java
@@ -0,0 +1,29 @@
+package com.example.algamoney.api;
+
+/*import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlgamoneyApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
